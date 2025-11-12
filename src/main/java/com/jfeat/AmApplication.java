5c460ba377c805aec531868acd86e618354f773b
Mac-Mini <ehuhaim@mac-mini.local>
2025-11-12 18:37:49 +0800

    buil: pass login

--- src/main/java/com/jfeat/AmApplication.java
+++ src/main/java/com/jfeat/AmApplication.java
@@ -1,11 +1,16 @@
 package com.jfeat;
 
+
+import com.jfeat.crud.plus.META;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * SpringBoot方式启动类
@@ -14,13 +19,16 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @Date 2017/5/21 12:06
  */
 @Slf4j
-@EnableScheduling
 @SpringBootApplication
-public class AmApplication{
+@EnableScheduling
+@EnableSwagger2
+@EnableFeignClients
+public class AmApplication extends WebMvcConfigurerAdapter {
 
     // protected final static Logger log = LoggerFactory.getLogger(AmApplication.class);
 
     public static void main(String[] args) {
+        META.enabledEav(true);
         SpringApplication.run(AmApplication.class, args);
         log.info("UAAS-APP running success!");
     }
